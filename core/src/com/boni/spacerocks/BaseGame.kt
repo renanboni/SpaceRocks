23a0be3c876fb3353f301cf1c609445fd3ad476d
renanboni <renan.boni@usp.br>
2019-12-14 00:24:42 -0300

    Handling discret events

--- core/src/com/boni/spacerocks/BaseGame.kt
+++ core/src/com/boni/spacerocks/BaseGame.kt
@@ -1,11 +1,17 @@
 package com.boni.spacerocks
 
 import com.badlogic.gdx.Game
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.InputMultiplexer
 
 abstract class BaseGame : Game() {
 
     init {
         game = this
+    }
+
+    override fun create() {
+        Gdx.input.inputProcessor = InputMultiplexer()
     }
 
     companion object {
